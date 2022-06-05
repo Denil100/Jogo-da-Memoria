8009613f3b3bd41196ce40589062b66d7d0d9807
Denil100 <ademilson_duartee@yahoo.com>
2022-06-05 18:23:41 -0300

    Ajustes Finais, Embaralhamento das Cartas

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 const cards = document.querySelectorAll('.card');
-let hasFlipperCard = false;
+let hasFlippedCard = false;
 let firstCard, secondCard;
 let lockBoard = false;
 
 function flipCard() {
     if(lockBoard) return;
-    
+    if(this === firstCard) return;
+
     this.classList.add('flip');
-    if(hasFlipperCard) {
-        hasFlipperCard = true;
+    if(!hasFlippedCard) {
+        hasFlippedCard = true;
         firstCard = this;
         return;
     }
 
     secondCard = this;
-    hasFlipperCard = false;
+    hasFlippedCard = false;
     checkForMatch();
 }
 
@@ -30,14 +31,32 @@ function checkForMatch() {
 function disableCards() {
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click', flipCard);
+    
+    resetBoard();
 }
 
 function unflipCards() {
+    lockBoard = true;
+
     setTimeout(() => {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
+
+        resetBoard();
     }, 1500);
 }
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+    cards.forEach((card) => {
+        let ramdomPosition = Math.floor(Math.random() *12);
+        card.style.order = ramdomPosition;
+    })
+})();
 
 cards.forEach((card) => {
     card.addEventListener('click', flipCard)
